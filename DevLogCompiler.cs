1cc60af349ac72c919abcb39cdeae8f4ce333ab3
Tricky1975 <facebook@tbbs.nl>
2018-11-16 21:05:18 +0100

    Initial

---
+++ DevLogCompiler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TrickyUnits;
+
+namespace DevLogCompiler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("DevLog ");
+        }
+    }
+}
